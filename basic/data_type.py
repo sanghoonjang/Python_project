628d023cd79fd94753349e1952eb18f69fa21449
Ray Jang <avalanche.jang@gmail.com>
2017-10-26 20:40:14 +0900

    Update and rename data_type to data_type.py

---
+++ basic/data_type.py
@@ -0,0 +1,20 @@
+# 변수
+pi = 3.141592
+
+# 문자열
+spam = "spam"
+
+# 리스트
+temp = [25.5, 36.5, 10, 43, 27.1]
+temp[0] # 25.5
+
+
+# 딕셔너리
+
+# set
+
+# tuple
+month_name = ("January", "February", "March", "April", "May", "June", "July")
+month_name[1] # February
+
+month_name[0] = "hello" # Error
