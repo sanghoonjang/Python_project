60726a5f6708655e8945783653c468141256aa13
Ray Jang <avalanche.jang@gmail.com>
2017-10-26 21:07:41 +0900

    Update data_type.py

--- basic/data_type.py
+++ basic/data_type.py
@@ -3,15 +3,61 @@ pi = 3.141592
 
 # 문자열
 spam = "spam"
+spam2 = "sspam"
+
+spam + spam2 # spamsspam
 
 # 리스트
 temp = [25.5, 36.5, 10, 43, 27.1]
+
 temp[0] # 25.5
 
+temp[0] = 40
+temp = [40, 36.5, 10, 43, 27.1]
+
+del temp[0]
+temp = [36.5, 10, 43, 27.1]
+
+temp2 = temp[1:3]
+temp2 # [10, 43, 27.1]
+
+temp[:2] # [36.5, 10, 43]
+temp[1:] # [10, 43, 27.1]
+
+city_temps = [
+  [14.0, 14.8, 15.1, 15.4, 15.2, 15.4, 17.0, 16.9]
+  [10.0, 10.4, 11.5, 11.2, 10.9, 10.6, 11.8, 13.2]
+  [16.0, 15.5, 16.4, 17.1, 14.2, 10.5, 12.6, 15.1]
+]
+city_temps[1] # [14.8, 14.8, 15.1, 15.4, 15.2, 15.4, 17.0, 16.9]
+city_temps[1][1] # 14.8
+
+team = [150, 160, 170, 180]
+sum(team) # 660
+min(team) # 150
+max(team) # 180
 
 # 딕셔너리
+dic = {1:"apple",
+       2:"banana",
+       3:"orange",
+       4:"kiwi"}
+
+dic[1] # apple
+
+dic[1] = "mango"
+dic # {1:"mango", 2:"banana", 3:"orange", 4:"kiwi"}
+
+dic[5] = "apple"
+dic # {1:"mango", 2:"banana", 3:"orange", 4:"kiwi", 5:"apple"}
+
+del dic[5]
+dic # {1:"mango", 2:"banana", 3:"orange", 4:"kiwi"}
 
 # set
+li = [1,1,1,2,3,4,5]
+seet = set(li)
+seet # {1,2,3,4,5}
 
 # tuple
 month_name = ("January", "February", "March", "April", "May", "June", "July")
