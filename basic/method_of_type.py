068ce5e4a0ef0e959da02116cf44c36ebede5f8e
Ray Jang <avalanche.jang@gmail.com>
2017-10-27 11:14:13 +0900

    Update method_of_type.py

--- basic/method_of_type.py
+++ basic/method_of_type.py
@@ -31,9 +31,48 @@ str_lower = str_upper.lower() # "a b c d e"
 "{} love you".format("i") # "i love you"
 
 #리스트
+team = [178, 154, 160, 180, 142]
+
+team.sort()
+team # [142, 154, 160, 178, 180]
+
+li = [1,2,3,4,5]
+li.reverse() # [5,4,3,2,1]
+li.remove(3) # [5,4,2,1]
+li.append(1) # [5,4,2,1,1]
+li.extend([2,3,4]) # [5,4,2,1,1,2,3,4]
+li.pop() # 4
+li # [5,4,2,1,1,2,3]
+li.index(5) # 0
 
 #딕셔너리
+dic = {1:"apple",
+       2:"banana",
+       3:"orange",
+       4:"kiwi"}
+
+dic.update({1:"abokado", 5:"kaki"})
+dic # {1: 'abokado', 2: 'banana', 3: 'orange', 4: 'kiwi', 5: 'kaki'}
+
+dic.keys() # dict_keys([1, 2, 3, 4, 5])
+
+dic.get(1) # "abokado"
+dic.get(10,"empty") # empty
+dic # {1: 'abokado', 2: 'banana', 3: 'orange', 4: 'kiwi', 5: 'kaki'}
+
+dic.setdefault(1) # "abokado"
+dic.setdefault(10, "empty")
+dic # {1: 'abokado', 2: 'banana', 3: 'orange', 4: 'kiwi', 5: 'kaki', 10: 'empty'}
+
+dic.items() # dict_items([(1, 'abokado'), (2, 'banana'), (3, 'orange'), (4, 'kiwi'), (5, 'kaki'), (10, 'empty')])
+
+dic.values() # dict_values(['abokado', 'banana', 'orange', 'kiwi', 'kaki', 'empty'])
 
 #set
+seet = {1,2,3,4,5}
 
-#tuple
+seet.add(5)
+seet # {1,2,3,4,5}
+
+seet.remove(5)
+seet # {1,2,3,4}
