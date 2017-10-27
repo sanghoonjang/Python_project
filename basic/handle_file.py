d291a5cd013c9f71767962ed217a6f19e984a557
Ray Jang <avalanche.jang@gmail.com>
2017-10-27 12:11:17 +0900

    Create handle_file.py

---
+++ basic/handle_file.py
@@ -0,0 +1,26 @@
+# test.txt
+# aaaaaaaa
+# bbbbbbbb
+# cccccccc
+# dddddddd
+
+
+# Text file
+f = open("test.txt", "r", encoding="utf-8")
+s = f.read()
+print(s)
+f.close()
+
+f.readline()
+f.readlines()
+
+f = open("test2.txt", "w", encoding="utf-8")
+f.write("ffffffffffff")
+f.close()
+
+# Binary file
+imgfile = open("test.png", "rb")
+imgsrc = imgfile.read()
+if imgsrc[1:4] == b"PNG":
+  print("image/png")
+
