42fd3d90687f9ef1d9602f32ccb657893386ff44
Ray Jang <avalanche.jang@gmail.com>
2017-10-27 12:22:12 +0900

    Create class.py

---
+++ basic/class.py
@@ -0,0 +1,12 @@
+# Make Class
+class Prism:
+  def __init__(self, width, height, depth):
+    self.width = width
+    self.height = height
+    self.depth = depth
+   
+  def content(self):
+    return self.width*self.height*self.depth
+ 
+
+  
