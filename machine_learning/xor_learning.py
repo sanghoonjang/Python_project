e3a5e962052883c4dea64a30fca49d515260064e
Ray Jang <avalanche.jang@gmail.com>
2017-10-28 11:59:52 +0900

    Update xor_learning.py

--- machine_learning/xor_learning.py
+++ machine_learning/xor_learning.py
@@ -31,4 +31,26 @@ for idx, answer in enumerate(label):
   total += 1
   
 print("a percentage of correct answers :", ok "/" total, "=", ok/total)
-  
+
+
+# Use Pandas
+import pandas as pd
+from sklearn import svm, metrics
+
+xor_input = [
+  [0, 0, 0],
+  [0, 1, 1],
+  [1, 0, 1],
+  [1, 1, 0]
+]
+
+xor_df = pd.DataFrame(xor_input)
+xor_data = xor_df.ix[:,0:1]
+xor_label = xor_df.ix[:,2]
+
+clf = svm.SVC()
+clf.fit(xor_data, xor_label)
+pre = clf.predict(xor_data)
+
+ac_score = metrics.accuracy_score(xor_label, pre)
+print("a percentage of correct answers :", ac_score)
