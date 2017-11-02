60b0566d5a6a7d377ba85360130e81126b98720a
Ray Jang <avalanche.jang@gmail.com>
2017-11-02 18:21:22 +0900

    Update pandas_in_text.py

--- pandas_in_text.py
+++ pandas_in_text.py
@@ -1,1 +1,18 @@
 
+import pandas as pd
+
+
+# Set A DataFrame
+var = [1,2,3,4,5]
+
+df = pd.DataFrame(var)
+df # 
+
+# Change column's name
+df.columns = "var"
+
+# Adding column
+df["num"] = 1
+
+# Delete a column
+del df["num"]
