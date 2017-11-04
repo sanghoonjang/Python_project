08520e02b9db63c4d84ce63dc56bd1e8512de93e
Ray Jang <avalanche.jang@gmail.com>
2017-11-04 17:43:12 +0900

    Update pandas_in_text.py

--- pandas_in_text.py
+++ pandas_in_text.py
@@ -11,13 +11,20 @@ df """   0
       1  2
       2  3
       3  4
-      4  5 """
+      4  5 
+   """
 
 # Change column's name
 df.columns = ['var']
 
+
 # Adding column
 df["num"] = 1
 
+
 # Delete a column
 del df["num"]
+
+
+# Export to CSV
+df.to_csv('fail_name',index=False)
