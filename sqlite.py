0c5f34b7fa2f84387806bed8ff466cbfad0dd70b
Ray Jang <avalanche.jang@gmail.com>
2017-11-04 19:07:48 +0900

    Update sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -1,1 +1,17 @@
 
+import sqlite3
+
+# make file.db
+conn = sqlite3.connect('example.db')
+
+c = conn.cousor()
+
+c.execute('''CREATE TABLE items ()''')
+
+c.execute("INSERT INTO items VALUES ()")
+
+conn.commit()
+conn.close()
+
+c.execute('SELECT * FROM items ORDER BY price')
+
